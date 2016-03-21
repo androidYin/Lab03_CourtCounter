69efe1ef9b0100cd50be97fbc145c7647b182a0d
androidYin <teacheryin.android@gmail.com>
2016-03-21 12:05:38 +0800

    Fragment implements View.OnClickListener

---
+++ app/src/main/java/com/teacheryin/android/lab03_courtcounter/CourtCounterFragment.java
@@ -0,0 +1,51 @@
+package com.teacheryin.android.lab03_courtcounter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CourtCounterFragment extends Fragment implements View.OnClickListener {
+
+    private TextView m_tv_team_name;
+    private TextView m_tv_team_score;
+    private Button m_btn_three_points;
+
+    public CourtCounterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment (產生 Fragment 回傳給 Activity)
+        return inflater.inflate(R.layout.fragment_court_counter, container, false);
+    }
+
+    @Override
+    public void onStart() { // Fragment 畫面建立後執行
+        super.onStart();
+
+        m_tv_team_name = (TextView)getView().findViewById(R.id.tv_team_name);
+        m_tv_team_score = (TextView)getView().findViewById(R.id.tv_team_score);
+        m_btn_three_points = (Button)getView().findViewById(R.id.btn_three_points);
+        m_btn_three_points.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int score = Integer.parseInt(m_tv_team_score.getText().toString());
+        m_tv_team_score.setText(String.valueOf(score + 3));
+    }
+}
--- app/src/main/java/com/teacheryin/android/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/teacheryin/android/lab03_courtcounter/MainActivity.java
@@ -1,9 +1,8 @@
 package com.teacheryin.android.lab03_courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,51 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-
-    }
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-
-    }
-
-    public void add3ForTeamA(View view) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add2ForTeamA(View view) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add1ForTeamA(View view) {
-        scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void add3ForTeamB(View view) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add2ForTeamB(View view) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void add1ForTeamB(View view) {
-        scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
-    }
 
     public void resetScoreView(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+
+        // 重設 Fragment Score 為 0
+        // ...
     }
+
 }
